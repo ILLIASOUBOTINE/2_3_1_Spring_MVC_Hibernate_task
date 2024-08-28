c647360c46b609caf5dbf826bb00d1f7074a34bb
LLIASOUBOTINE <semenuk1991311@gmail.com>
2024-08-29 01:05:41 +0300

    first commit

---
+++ src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,49 @@
+package web.dao;
+
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @PersistenceContext
+   private EntityManager entityManager;
+
+
+    @Override
+    public void add(User user) {
+     entityManager.persist(user);
+    }
+
+   @Override
+   public List<User> listUsers() {
+      TypedQuery<User> query = entityManager.createQuery("from User", User.class);
+      return query.getResultList();
+   }
+
+   @Override
+   public void update(User user) {
+      entityManager.merge(user);
+   }
+
+   @Override
+   public void delete(Long id) {
+      User user = entityManager.find(User.class, id);
+      if (user != null) {
+         entityManager.remove(user);
+      }
+
+   }
+
+   @Override
+   public User findById(Long id) {
+      return entityManager.find(User.class, id);
+   }
+
+
+}
